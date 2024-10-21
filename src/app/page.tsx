3d61776b3359fd0d2918fe764337f13d147c5193
Abhay <patilabhay1357@gmail.com>
2024-10-21 17:04:42 +0530

    [Bug Fixes]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <button className="text-white absolute left-10 ml-32 pl-32 mt-7">Features</button>
       <button className="text-white absolute left-20 ml-32 pl-48 mt-7">Customers</button>
       <button className="text-white absolute right-10 mr-8 mt-6  rounded-xl bg-blueish-100 w-14 h-8">Login</button>
-      <button className="text-white absolute right-20 mr-16 mt-6  rounded-xl bg-blueish-100 w-16 h-8">Signup</button>
+      <button className="text-white absolute right-20 mr-16 mt-6  rounded-xl bg-blueish-100 w-16 h-8 md:hidden">Signup</button>
     </nav>
   );
 }
