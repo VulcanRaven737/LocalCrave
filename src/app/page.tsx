551ef0a6d5a58ed747e30d0377152359e709defc
Abhay <patilabhay1357@gmail.com>
2024-10-23 23:35:36 +0530

    [Bug Fixes]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <Head>
         <style>
-        @import url('https://fonts.googleapis.com/css2?family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+          @import url('https://fonts.googleapis.com/css2?family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
         </style>
       </Head>
       <nav className="p-3 bg-[#ffff] flex relative">
@@ -17,14 +17,14 @@ export default function Home() {
           <button className="text-[#ffff] bg-black absolute font-thin text-xs right-20 mt-6 rounded w-16 h-8" onClick={() => window.location.href='/features'}>Sign In</button>
       </nav>
       <div className='mt-24 flex font-poppins text-6xl subpixel-antialiased font-normal flex'>
-        <Image src='images/semicylinder.svg' alt='semi'/>
+        <Image src='images/semicylinder.svg' width={182} height={366} alt='semi'/>
         <div className='mt-12 pl-3'>
           <span>Premium<span className='ml-4 text-[#FC8019] font-bold'>quality<br/>home-cooked</span><span className='ml-3 text-black'>meals</span></span><br/><br/>
           <span className='text-lg font-poppins text-[#404040]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br/> tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim,<br/> quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</span>
         </div>
-        <Image src='images/rectangle1.svg' alt='rect1' className='ml-48'/>
-        <Image src='images/rectangle2.svg' alt='rect2' className='ml-20'/>
-        <Image src='images/rectangle3.svg' alt='rect3' className='ml-20 pl-2'/>
+        <Image src='images/rectangle1.svg' alt='rect1' width={400} height={600} className='ml-60'/>
+        <Image src='images/rectangle2.svg' alt='rect2' width={300} height={450} className='ml-20'/>
+        <Image src='images/rectangle3.svg' alt='rect3' width={49} height={450} className='ml-20'/>
       </div>
     </>
   );
