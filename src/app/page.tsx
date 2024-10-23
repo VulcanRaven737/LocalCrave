7d8f176dfd714b89e91775620a77f8f54b876552
Abhay <patilabhay1357@gmail.com>
2024-10-23 23:26:41 +0530

    [Bug Fixes]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import './globals.css'
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -11,19 +12,19 @@ export default function Home() {
         </style>
       </Head>
       <nav className="p-3 bg-[#ffff] flex relative">
-          <img src='icons/main-logo.svg' width={200} height={200} className='ml-28 mt-2' onClick={() => window.location.href='/'}/>
-          <img src='icons/bag.svg' width={23} height={23} className='absolute right-40 mt-6 pt-1 mr-3'/>
+          <Image src='icons/main-logo.svg' alt='logo' width={200} height={200} className='ml-28 mt-2' onClick={() => window.location.href='/'}/>
+          <Image src='icons/bag.svg' alt='bag' width={23} height={23} className='absolute right-40 mt-6 pt-1 mr-3'/>
           <button className="text-[#ffff] bg-black absolute font-thin text-xs right-20 mt-6 rounded w-16 h-8" onClick={() => window.location.href='/features'}>Sign In</button>
       </nav>
       <div className='mt-24 flex font-poppins text-6xl subpixel-antialiased font-normal flex'>
-        <img src='images/semicylinder.svg'/>
+        <Image src='images/semicylinder.svg' alt='semi'/>
         <div className='mt-12 pl-3'>
           <span>Premium<span className='ml-4 text-[#FC8019] font-bold'>quality<br/>home-cooked</span><span className='ml-3 text-black'>meals</span></span><br/><br/>
           <span className='text-lg font-poppins text-[#404040]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br/> tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim,<br/> quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</span>
         </div>
-        <img src='images/rectangle1.svg' className='ml-48'/>
-        <img src='images/rectangle2.svg' className='ml-20'/>
-        <img src='images/rectangle3.svg' className='ml-20 pl-2'/>
+        <Image src='images/rectangle1.svg' alt='rect1' className='ml-48'/>
+        <Image src='images/rectangle2.svg' alt='rect2' className='ml-20'/>
+        <Image src='images/rectangle3.svg' alt='rect3' className='ml-20 pl-2'/>
       </div>
     </>
   );
